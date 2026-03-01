708d7fe18898c190a1cf5dbc620443b5b128b008
Imam Hudzaifah <imamhudzaifah30@gmail.com>
2026-03-01 22:13:08 +0800

    menambahkan-merapihkan

--- src/Tombol.jsx
+++ src/Tombol.jsx
@@ -1,23 +1,26 @@
 function Tombol(props) {
   return (
-    <div>
-      {/* Tombol akan 'disabled' (mati) jika jumlah saat ini adalah 0 */}
+    <div className="button-group">
       <button 
+        className="kurang"
         onClick={props.kurang} 
         disabled={props.jumlahSaatIni === 0}
       >
         Kurangi
       </button>
 
-      {/* Tombol akan 'disabled' jika jumlah mencapai batas maksimal */}
       <button 
+        className="tambah"
         onClick={props.tambah} 
         disabled={props.jumlahSaatIni === props.batas}
       >
         Tambah
       </button>
 
-      <button onClick={props.reset}>
+      <button 
+        className="reset"
+        onClick={props.reset}
+      >
         Kosongkan Keranjang
       </button>
     </div>
